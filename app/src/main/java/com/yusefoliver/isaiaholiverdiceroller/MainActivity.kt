c82d5bed8f0e5b18c4929057fe4b993749512567
yusefoliver <isaiahyoliver@gmail.com>
2025-05-06 18:28:02 -0500

    Add some space between label and button

--- app/src/main/java/com/yusefoliver/isaiaholiverdiceroller/MainActivity.kt
+++ app/src/main/java/com/yusefoliver/isaiaholiverdiceroller/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.Modifier
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.ui.unit.dp
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
@@ -20,6 +21,7 @@ class MainActivity : ComponentActivity() {
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text("Dice Roll: $diceRoll", style = MaterialTheme.typography.headlineMedium)
+                Spacer(modifier = Modifier.height(16.dp))
                 Button(onClick = { diceRoll = Random.nextInt(1, 7) }) {
                     Text("Roll Dice")
                 }
